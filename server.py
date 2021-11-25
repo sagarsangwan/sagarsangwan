27d4a2df86df20c39ead1750f057cff26089b729
sagarsangwan <iamsagarsangwan@gmail.com>
2021-11-26 02:56:55 +0530

    base html file added for all pages

--- server.py
+++ server.py
@@ -9,6 +9,14 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
                         
