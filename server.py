76883e0d7c5c47dd0a25797b401a5c4fbfe8e0ef
sagarsangwan <iamsagarsangwan@gmail.com>
2021-11-27 10:41:05 +0530

    new page added

--- server.py
+++ server.py
@@ -9,6 +9,10 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/contact_us")
+def contact_us():
+    return render_template("contact_us.html")
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"),404
