b539b994b7d4617457df066da62b7fd703c69d4b
sagarsangwan <iamsagarsangwan@gmail.com>
2021-11-29 14:04:22 +0530

    new folder added

--- server.py
+++ server.py
@@ -6,28 +6,28 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("pages/home.html")
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("pages/about.html")
 
 
 @app.route("/contact", methods=['GET', 'POST'])
 def contact():
 
-    return render_template("contact.html")
+    return render_template("pages/contact.html")
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"), 404
+    return render_template("pages/404.html"), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template("500.html")
+    return render_template("pages/500.html")
 
 
 if __name__ == "__main__":
