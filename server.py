686bde4d275db8e79f2902eb6a39b08678ceff77
sagarsangwan <iamsagarsangwan@gmail.com>
2021-11-29 11:15:58 +0530

    cleaned virtual envs

--- server.py
+++ server.py
@@ -4,27 +4,31 @@ from flask import Flask, app, render_template
 app = Flask(__name__)
 
 
-
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route("/contact", methods = ['GET', 'POST'])
+
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
 
     return render_template("contact.html")
 
+
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"),404
+    return render_template("404.html"), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template("500.html")
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-                        
